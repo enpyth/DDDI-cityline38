bc2b6890d8caed20ed7abb67426605738e194c7a
yyx201601 <161414070+yyx201601@users.noreply.github.com>
2026-01-08 17:03:49 +1030

    Change video size

--- src/features/home/LifeStyle.tsx
+++ src/features/home/LifeStyle.tsx
@@ -51,7 +51,7 @@ export default function LifeStyle() {
                         muted
                         playsInline
                         style={{
-                            width: '100%',
+                            width: '62%',
                             height: 'auto',
                             display: 'block'
                         }}
